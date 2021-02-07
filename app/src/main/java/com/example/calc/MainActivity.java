0bce365c3f8fade92c0acd8dd50c64bfc665a60f
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-02-07 21:32:41 +0200

    Updated-4.

--- app/src/main/java/com/example/calc/MainActivity.java
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.calc;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
@@ -25,9 +23,9 @@ public class MainActivity extends AppCompatActivity
         btn5_ID = (Button) findViewById(R.id.btn5);
         btn6_ID = (Button) findViewById(R.id.btn6);
         btn7_ID = (Button) findViewById(R.id.btn7);
-        btn9_ID = (Button) findViewById(R.id.btn9);
-        btn10_ID = (Button) findViewById(R.id.btn10);
-        btn11_ID = (Button) findViewById(R.id.btn11);
+        btn9_ID = (Button) findViewById(R.id.btn20);
+        btn10_ID = (Button) findViewById(R.id.btn23);
+        btn11_ID = (Button) findViewById(R.id.btn22);
         tv1_ID = (TextView) findViewById(R.id.tv1);
 
         btn1_ID.setOnClickListener(new View.OnClickListener()
