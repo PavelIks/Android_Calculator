fde6b39151f6d390fea42352ee759c4c020d48c9
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-02-07 21:58:57 +0200

    Updated-6.

--- app/src/main/java/com/example/calc/MainActivity.java
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -18,6 +18,14 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         btn5_ID = (Button) findViewById(R.id.btn5);
+        btn6_ID = (Button) findViewById(R.id.btn6);
+        btn7_ID = (Button) findViewById(R.id.btn7);
+        btn9_ID = (Button) findViewById(R.id.btn9);
+        btn10_ID = (Button) findViewById(R.id.btn10);
+        btn11_ID = (Button) findViewById(R.id.btn11);
+        btn13_ID = (Button) findViewById(R.id.btn13);
+        btn14_ID = (Button) findViewById(R.id.btn14);
+        btn15_ID = (Button) findViewById(R.id.btn15);
         tv1_ID = (TextView) findViewById(R.id.tv1);
 
         btn5_ID.setOnClickListener(new View.OnClickListener()
@@ -25,7 +33,79 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
+                tv1_ID.setText(tv1_ID.getText() + "7");
+            }
+        });
+
+        btn6_ID.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                tv1_ID.setText(tv1_ID.getText() + "8");
+            }
+        });
+
+        btn7_ID.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                tv1_ID.setText(tv1_ID.getText() + "9");
+            }
+        });
+
+        btn9_ID.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                tv1_ID.setText(tv1_ID.getText() + "4");
+            }
+        });
+
+        btn10_ID.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                tv1_ID.setText(tv1_ID.getText() + "5");
+            }
+        });
+
+        btn11_ID.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                tv1_ID.setText(tv1_ID.getText() + "6");
+            }
+        });
+
+        btn13_ID.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
                 tv1_ID.setText(tv1_ID.getText() + "1");
+            }
+        });
+
+        btn14_ID.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                tv1_ID.setText(tv1_ID.getText() + "2");
+            }
+        });
+
+        btn15_ID.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                tv1_ID.setText(tv1_ID.getText() + "3");
             }
         });
 
