a1488cfec3ea04c2f0491d3562c6eb95ccf805b8
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-03-01 13:58:00 +0200

    Updated-12.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,86 +1,62 @@
 package com.example.myapplication;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.LinkedList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
     Double first=0.0;
     String operation="";
     Double second=0.0;
     double MRvalue=0.0;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-
         LinkedList<Button> buttons=new LinkedList<Button>();
-
-
-
 
         buttons.add(findViewById(R.id.button21));
         buttons.add(findViewById(R.id.button22));
         buttons.add(findViewById(R.id.button23));
 
-
-
-
         buttons.add(findViewById(R.id.button26));
         buttons.add(findViewById(R.id.button27));
         buttons.add(findViewById(R.id.button28));
-
 
         buttons.add(findViewById(R.id.button30));
         buttons.add(findViewById(R.id.button31));
         buttons.add(findViewById(R.id.button32));
         buttons.add(findViewById(R.id.button33));
 
-
-
-
-
-
-
-
-
-
-
-
-
-        for (Button item: buttons) {
+        for (Button item: buttons)
+        {
             item.setOnClickListener(new Button.OnClickListener()
             {
                 @Override
-                public void onClick(View v) {
+                public void onClick(View v)
+                {
                     ((TextView)findViewById(R.id.editTextTextPersonName)).setText(((TextView)findViewById(R.id.editTextTextPersonName)).getText().toString()+item.getText().toString());
                 }
             });
         }
 
-
-
         // .
         findViewById(R.id.button34).setOnClickListener(new Button.OnClickListener()
         {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 if(!((TextView)findViewById(R.id.editTextTextPersonName)).getText().toString().contains("."))
                 {
-                    ((TextView)findViewById(R.id.editTextTextPersonName)).setText(((TextView)findViewById(R.id.editTextTextPersonName)).getText().toString()+
-                            ((Button)findViewById(R.id.button34)).getText().toString());
+                    ((TextView)findViewById(R.id.editTextTextPersonName)).setText(((TextView)findViewById(R.id.editTextTextPersonName)).getText().toString() + ((Button)findViewById(R.id.button34)).getText().toString());
                 }
-
-
             }
         });
 
@@ -88,10 +64,10 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button37).setOnClickListener(new Button.OnClickListener()
         {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 operation=((TextView)findViewById(R.id.button37)).getText().toString();
                 findViewById(R.id.button35).callOnClick();
-
             }
         });
 
@@ -99,11 +75,10 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button38).setOnClickListener(new Button.OnClickListener()
         {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 operation=((TextView)findViewById(R.id.button38)).getText().toString();
                 findViewById(R.id.button35).callOnClick();
-
-
             }
         });
 
@@ -111,67 +86,65 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button39).setOnClickListener(new Button.OnClickListener()
         {
             @Override
-            public void onClick(View v) {
-                    operation=((TextView)findViewById(R.id.button39)).getText().toString();
+            public void onClick(View v)
+            {
+                operation=((TextView)findViewById(R.id.button39)).getText().toString();
                 findViewById(R.id.button35).callOnClick();
             }
         });
 
-
-//C
+        // C
         (findViewById(R.id.button36)).setOnClickListener(new Button.OnClickListener()
         {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 ((TextView)findViewById(R.id.editTextTextPersonName)).setText("");
             }
         });
 
-        //+
+        // +
         (findViewById(R.id.button29)).setOnClickListener(new Button.OnClickListener()
         {
             @Override
-            public void onClick(View v) {
-                if(first==0.0)
+            public void onClick(View v)
+            {
+                if (first == 0.0)
                 {
                     operation=((TextView)findViewById(R.id.button29)).getText().toString();
                     first= new Double(((TextView)findViewById(R.id.editTextTextPersonName)).getText().toString());
                     ((TextView)findViewById(R.id.editTextTextPersonName)).setText("");
                 }
-
-
             }
         });
 
-        //-
+        // -
         (findViewById(R.id.button24)).setOnClickListener(new Button.OnClickListener()
         {
             @Override
-            public void onClick(View v) {
-                if(first==0.0)
+            public void onClick(View v)
+            {
+                if(first == 0.0)
                 {
                     operation=((TextView)findViewById(R.id.button24)).getText().toString();
                     first= new Double(((TextView)findViewById(R.id.editTextTextPersonName)).getText().toString());
                     ((TextView)findViewById(R.id.editTextTextPersonName)).setText("");
                 }
-
-
             }
         });
 
-        //del
+        // del
         findViewById(R.id.button19).setOnClickListener(new Button.OnClickListener()
         {
             @Override
-            public void onClick(View v) {
-             if(((TextView)findViewById(R.id.editTextTextPersonName)).getText().toString().length()>0)
-             {
-                 String tmp=((TextView)findViewById(R.id.editTextTextPersonName)).getText().toString();
-                 tmp = tmp.substring(0, tmp.length() - 1);
-                 ((TextView)findViewById(R.id.editTextTextPersonName)).setText(tmp);
-             }
-
-
+            public void onClick(View v)
+            {
+                if(((TextView)findViewById(R.id.editTextTextPersonName)).getText().toString().length()>0)
+                {
+                    String tmp=((TextView)findViewById(R.id.editTextTextPersonName)).getText().toString();
+                    tmp = tmp.substring(0, tmp.length() - 1);
+                    ((TextView)findViewById(R.id.editTextTextPersonName)).setText(tmp);
+                }
             }
         });
 
@@ -179,88 +152,78 @@ public class MainActivity extends AppCompatActivity {
         (findViewById(R.id.button3)).setOnClickListener(new Button.OnClickListener()
         {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 if(first==0.0)
                 {
                     operation=((TextView)findViewById(R.id.button3)).getText().toString();
-                    first= new Double(((TextView)findViewById(R.id.editTextTextPersonName)).getText().toString());
+                    first = new Double(((TextView)findViewById(R.id.editTextTextPersonName)).getText().toString());
                     ((TextView)findViewById(R.id.editTextTextPersonName)).setText("");
                 }
-
-
             }
         });
-        //  %
+
+        // %
         (findViewById(R.id.button17)).setOnClickListener(new Button.OnClickListener()
         {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 if(first==0.0)
                 {
                     operation=((TextView)findViewById(R.id.button17)).getText().toString();
                     first= new Double(((TextView)findViewById(R.id.editTextTextPersonName)).getText().toString());
                     ((TextView)findViewById(R.id.editTextTextPersonName)).setText("");
                 }
-
-
             }
         });
 
-        //  +|-
+        // +|-
         (findViewById(R.id.button18)).setOnClickListener(new Button.OnClickListener()
         {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 if(first==0.0)
                 {
                     operation=((TextView)findViewById(R.id.button18)).getText().toString();
                     first= new Double(((TextView)findViewById(R.id.editTextTextPersonName)).getText().toString());
                     ((TextView)findViewById(R.id.editTextTextPersonName)).setText("");
                 }
-
-
             }
         });
-        //*
+
+        // *
         (findViewById(R.id.button20)).setOnClickListener(new Button.OnClickListener()
         {
             @Override
-            public void onClick(View v) {
-                if(first==0.0)
+            public void onClick(View v)
+            {
+                if(first == 0.0)
                 {
                     operation=((TextView)findViewById(R.id.button20)).getText().toString();
                     first= new Double(((TextView)findViewById(R.id.editTextTextPersonName)).getText().toString());
                     ((TextView)findViewById(R.id.editTextTextPersonName)).setText("");
                 }
-
-
             }
         });
-
-
-
-
-
     }
 
-    //= MR MC M+
+    // = MR MC M+
     public void OnRezultClick(View v)
     {
-        if(((TextView)findViewById(R.id.editTextTextPersonName)).getText().toString().length()>0) {
+        if(((TextView)findViewById(R.id.editTextTextPersonName)).getText().toString().length()>0)
+        {
             second = new Double(((TextView) findViewById(R.id.editTextTextPersonName)).getText().toString());
         }
 
         if((operation.contains("MC") || operation.contains("MR") || operation.contains("M+")) || (first!=0.0 && operation!=""))
         {
-
-
             switch (operation)
             {
                 case "+":
                 {
-
                     ((TextView)findViewById(R.id.editTextTextPersonName)).setText((String.valueOf(first+second)));
-
                 }
                 break;
                 case "/":
@@ -311,19 +274,15 @@ public class MainActivity extends AppCompatActivity {
                 break;
                 case "M+":
                 {
-
                     MRvalue+=second;
                     Toast.makeText(getApplicationContext(),"Memory-value now is "+MRvalue+"!", Toast.LENGTH_SHORT).show();
                 }
                 break;
             }
 
-
-
             first=0.0;
             second=0.0;
             operation="";
         }
-
     }
 }
